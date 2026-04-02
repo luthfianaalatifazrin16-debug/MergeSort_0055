fd35c224cd717654f9451a8e34ddaaace72ac778
luthfianaalatifazrin16-debug <luthfianaalatifazrin16@gmail.com>
2026-04-02 19:02:18 +0700

    while true

--- mergesort.cpp
+++ mergesort.cpp
@@ -7,3 +7,18 @@ int arr[20], B[20];
 int n;
 
 void input()
+{
+    while (true)
+    {
+        cout << "Masukan Panjang element array ; ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+    }
